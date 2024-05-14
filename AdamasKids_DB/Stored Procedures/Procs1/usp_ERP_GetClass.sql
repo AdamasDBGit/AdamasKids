a5a42f2b026723f6a586ac3cd8c4b0b35ae5fb43
AdamasDBGit <abhik.porel@adamastech.in>
2024-05-14 15:31:38 +0530

    add column s_location

--- AdamasKids_DB/Stored Procedures/Procs1/usp_ERP_GetClass.sql
+++ AdamasKids_DB/Stored Procedures/Procs1/usp_ERP_GetClass.sql
@@ -1,9 +1,10 @@
-﻿-- =============================================
+﻿
+-- =============================================
 -- Author:		<Parichoy Nandi>
 -- Create date: <14th September 2023>
 -- Description:	<to get the class>
 -- =============================================
-CREATE PROCEDURE [dbo].[usp_ERP_GetClass]
+Create PROCEDURE [dbo].[usp_ERP_GetClass]
 	@iClassID int = null,
 	@iBrandID int
 AS
@@ -16,7 +17,8 @@ BEGIN
 		TC.S_Class_Name as ClassName,
 		TC.I_Status as ClassStatus
 		from [dbo].[T_Class] as TC
-		where TC.I_Class_ID= ISNULL(@iClassID,TC.I_Class_ID) and I_Brand_ID = @iBrandID
+		where TC.I_Class_ID= ISNULL(@iClassID,TC.I_Class_ID)
+		and I_Brand_ID = @iBrandID
 		ORDER BY 
     TC.I_Class_ID ASC;
 	END
