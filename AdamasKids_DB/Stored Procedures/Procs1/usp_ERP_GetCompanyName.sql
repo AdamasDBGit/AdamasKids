4a759bfdd7b9f6d24089a82c218d459e5735f674
AdamasDBGit <abhik.porel@adamastech.in>
2024-05-22 15:31:37 +0530

    update changes from db-kids

--- AdamasKids_DB/Stored Procedures/Procs1/usp_ERP_GetCompanyName.sql
+++ AdamasKids_DB/Stored Procedures/Procs1/usp_ERP_GetCompanyName.sql
@@ -1,5 +1,4 @@
-﻿
-CREATE PROCEDURE [dbo].[usp_ERP_GetCompanyName] ---738  
+﻿CREATE PROCEDURE [dbo].[usp_ERP_GetCompanyName] ---738  
   
  @iCentreID INT  
   
@@ -8,17 +7,20 @@ BEGIN
   
  SET NOCOUNT OFF;  
   
- SELECT TCM.I_Centre_Id CentreId,TCM.S_Center_Name CenterName,TAC.I_Agreement_Center_ID AgreementCenterID,TBM.S_Brand_Name AS CompanyName 
- ,ECM.S_config_Value as ImageUrl
- FROM dbo.T_Centre_Master AS TCM  
- INNER JOIN NETWORK.T_Agreement_Center AS TAC  
- ON TCM.I_Centre_Id = TAC.I_Centre_Id  
- INNER JOIN NETWORK.T_Agreement_Details AS TAD  
- ON TAC.I_Agreement_ID = TAD.I_Agreement_ID  
- INNER JOIN dbo.T_Brand_Master TBM ON  TAD.I_Brand_ID = TBM.I_Brand_ID
- LEFT JOIN T_ERP_Configuration_Master as ECM 
- on ECM.I_Brand_ID=TBM.I_Brand_ID and ECM.S_config_code='BRAND_LOGO' and ECM.S_Screen IS NULL
- WHERE TCM.I_Centre_Id = @iCentreID  
+  select t2.S_Brand_Name  AS CompanyName
+  from T_Centre_Master t1 inner join T_Brand_Master t2 on t1.S_Center_Code=t2.S_Brand_Code 
+  where t1.I_Centre_Id = @iCentreID
+ --SELECT TCM.I_Centre_Id CentreId,TCM.S_Center_Name CenterName,TAC.I_Agreement_Center_ID AgreementCenterID,TBM.S_Brand_Name AS CompanyName 
+ --,ECM.S_config_Value as ImageUrl
+ --FROM dbo.T_Centre_Master AS TCM  
+ --INNER JOIN NETWORK.T_Agreement_Center AS TAC  
+ --ON TCM.I_Centre_Id = TAC.I_Centre_Id  
+ --INNER JOIN NETWORK.T_Agreement_Details AS TAD  
+ --ON TAC.I_Agreement_ID = TAD.I_Agreement_ID  
+ --INNER JOIN dbo.T_Brand_Master TBM ON  TAD.I_Brand_ID = TBM.I_Brand_ID
+ --LEFT JOIN T_ERP_Configuration_Master as ECM 
+ --on ECM.I_Brand_ID=TBM.I_Brand_ID and ECM.S_config_code='BRAND_LOGO' and ECM.S_Screen IS NULL
+ --WHERE TCM.I_Centre_Id = @iCentreID  
    
 END
 
---
+++ AdamasKids_DB/Tables/T_ERP_Brand_Info_Details.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[T_ERP_Brand_Info_Details] (
+    [BrandInfo_ID]  INT          IDENTITY (1, 1) NOT NULL,
+    [I_Brand_ID]    INT          NULL,
+    [s_GST_No]      VARCHAR (25) NULL,
+    [s_CIN_No]      VARCHAR (20) NULL,
+    [s_Pan_No]      VARCHAR (20) NULL,
+    [s_stateCode]   VARCHAR (5)  NULL,
+    [is_Active]     BIT          NULL,
+    [dt_created_dt] DATETIME     DEFAULT (getdate()) NULL
+);
+
